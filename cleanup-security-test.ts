254401bd08963a93f64bb6689a38a121f751c59e
varaprasad valluri <41137518+varaprasadvalluri@users.noreply.github.com>
2026-08-24 17:57:27 +0530

    Add IDE project config and local cleanup script
    - .idea/, .vscode/, android/.idea/: editor project settings
    - cleanup-security-test.ts: one-off script to remove specific test docs

---
+++ cleanup-security-test.ts
@@ -0,0 +1,9 @@
+import './server/loadEnv';
+import { clientDb, clientDoc, clientDeleteDoc } from './server/firestoreClient';
+
+async function main() {
+  await clientDeleteDoc(clientDoc(clientDb, 'users', 'edu-usr-fc73396a75e6-mt5k593o'));
+  await clientDeleteDoc(clientDoc(clientDb, 'attempts', 'att_nonexistent-exam-for-test_edu-usr-fc73396a75e6-mt5k593o'));
+  console.log('Cleaned up test docs.');
+}
+main().then(() => process.exit(0)).catch((e) => { console.error(e); process.exit(1); });
